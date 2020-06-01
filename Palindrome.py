e9ef72461f8db81b06063fb384e2512a8bcfc5d1
Clement Darryl <48811307+clementmac@users.noreply.github.com>
2020-06-01 14:43:00 -0400

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,8 @@
+num = 'bubbles'
+x = str(num)
+if str(num)[::-1] == x:
+    print("true")
+else:
+    print("false")
+
+
---
+++ Reverse.py
@@ -0,0 +1,14 @@
+
+def rev_num():
+    num = 894
+    rev = 0
+
+    while num > 0:
+         a = num % 10
+         rev = rev*10 + a
+         num = num // 10
+
+    print(rev)
+rev_num()
+
+
